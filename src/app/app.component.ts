62e1a8117ca0fd9c91aed0c8771dc2c39050b202
mattjcarkeek <114029419+mattjcarkeek@users.noreply.github.com>
2024-10-01 12:15:51 +1300

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'toolbox';
+  title = 'Tools at your disposal:';
+  websites = [
+    { 
+      name: 'Sticker Generator', 
+      url: 'https://mattjcarkeek.github.io/stickergenerator/',
+      description: 'Scan To Play Stickers',
+      previewUrl: `https://mini.s-shot.ru/1024x768/JPEG/1024/Z100/?https://mattjcarkeek.github.io/stickergenerator/`
+    },
+    { 
+      name: 'Blog Copy-erator', 
+      url: 'https://mattjcarkeek.github.io/sitecopy/',
+      description: "Copy and AMS'ify blogs",
+      previewUrl: `https://mini.s-shot.ru/1024x768/JPEG/1024/Z100/?https://mattjcarkeek.github.io/sitecopy/`
+    }
+  ];
 }
