c64729baac6c491b26003e50add4aecbf2902f72
mattjcarkeek <114029419+mattjcarkeek@users.noreply.github.com>
2024-10-17 16:39:23 +1300

    add lone star

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,12 @@ export class AppComponent {
       url: 'https://mattjcarkeek.github.io/sitecopy/',
       description: "Copy and AMS'ify blogs",
       previewUrl: `https://mini.s-shot.ru/1024x768/JPEG/1024/Z100/?https://mattjcarkeek.github.io/sitecopy/`
+    },
+    { 
+      name: 'Blog Copy-erator', 
+      url: 'https://mattjcarkeek.github.io/songsearcher/',
+      description: "Song Deletion for Lone Star",
+      previewUrl: `https://mini.s-shot.ru/1024x768/JPEG/1024/Z100/?https://mattjcarkeek.github.io/songsearcher/`
     }
   ];
 }
